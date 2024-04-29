18b6fb3af32d851df89e01c8e9559b9436d0cc7b
pikapikamart <itsmelight1@gmail.com>
2024-04-29 18:59:04 +0800

    created migrations and schemas for user

--- src/app/_server/database/schema/users.ts
+++ src/app/_server/database/schema/users.ts
@@ -1,11 +1,17 @@
 import { 
   mysqlTable, 
-  serial, 
+  timestamp, 
   varchar } from "drizzle-orm/mysql-core";
 
-  
-export const users = mysqlTable("users", {
-  id: serial("id").primaryKey(),
-  name: varchar("name", { length: 256 }).notNull(),
-  email: varchar("email", { length: 256 }).notNull().unique(),
-});
+
+export const users = mysqlTable("user", {
+  id: varchar("id", { length: 255 })
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  name: varchar("name", { length: 255 }),
+  email: varchar("email", { length: 255 }).notNull(),
+  emailVerified: timestamp("emailVerified", {
+    mode: "date",
+    fsp: 3,
+  }),
+})
