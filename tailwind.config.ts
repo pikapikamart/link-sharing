ee6473a549dd514c2c7aaf95fb4bdd3d0b416e91
pikapikamart <itsmelight1@gmail.com>
2024-04-30 11:42:57 +0800

    created register page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,20 +15,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "purple": "#633CFF",
+        "purple-hover": "#BEADFF",
+        "light-purple": "#EFEBFF",
+        "dark-grey": "#333333",
+        "grey": "#737373",
+        "borders": "#D9D9D9",
+        "light-grey": "#FAFAFA",
+        "red": "#FF3939"
+      }
     },
     fontFamily: {
       instrument: ["var(--font-instrument_sans)"],
     },
-    colors: {
-      "blue-one": "#633CFF",
-      "violet-one": "#BEADFF",
-      "violet-two": "#EFEBFF",
-      "dark-one": "#333333",
-      "dark-two": "#737373",
-      "gray-one": "#D9D9D9",
-      "gray-two": "#FAFAFA",
-      "red-one": "#FF3939"
-    }
   },
   plugins: [],
 };
