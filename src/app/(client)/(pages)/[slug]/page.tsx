129123847b3246e2e1d17d819a50cc1acf4f69ee
pikapikamart <itsmelight1@gmail.com>
2024-05-03 17:08:13 +0800

    added username for the users table and made links cascade

---
+++ src/app/(client)/(pages)/[slug]/page.tsx
@@ -0,0 +1,11 @@
+
+
+const Page = () =>{
+
+  return (
+    <main></main>
+  )
+}
+
+
+export default Page
--- types/utils.ts
+++ types/utils.ts
@@ -2,4 +2,9 @@
 
 export type MakeWritable<T> = { -readonly [P in keyof T]: T[P] };
 export type GetElementType<T extends any[]> = T extends (infer U)[]? U :never
- 
+
+export type PageSlug = {
+  params: {
+    slug: string
+  }
+}
