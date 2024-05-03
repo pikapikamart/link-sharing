e1c419e94fa600d792eed3ef02233a161c97578b
pikapikamart <itsmelight1@gmail.com>
2024-05-03 19:08:00 +0800

    editted the not-found page

--- src/app/(client)/(pages)/[slug]/not-found.tsx
+++ src/app/(client)/(pages)/[slug]/not-found.tsx
@@ -1,14 +1,13 @@
-import Link from 'next/link'
  
 
 const NotFound = () => {
 
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/register">Return Home</Link>
-    </div>
+    <main className="min-h-screen w-full flex flex-col justify-center items-center bg-[#1A2238] px-2 text-center">
+      <h1 className="text-8xl font-extrabold text-white tracking-widest mb-2 md:text-9xl">404</h1>
+      <p className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">Page Not Found</p>
+      <p className="text-gray-500">Make sure to properly check the username</p>
+    </main>
   )
 }
 
