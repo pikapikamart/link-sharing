f68bb45703791fe830e3d8e8da944311e5e06eca
pikapikamart <itsmelight1@gmail.com>
2024-05-05 10:01:11 +0800

    removed placeholder background for the profile

--- src/app/(client)/_components/shared/profile/profile.tsx
+++ src/app/(client)/_components/shared/profile/profile.tsx
@@ -53,32 +53,18 @@ const Profile = ({ user }: ProfileProps) =>{
   return (
     <div className="max-w-[350px] mx-auto rounded-3xl md:bg-white md:py-12 md:px-14 md:shadow-lg">
       <div className="mb-14 text-center"> 
+      { !!user.image && (
         <div className="rounded-full border-4 max-w-max mx-auto border-purple mb-6">
-          { !!user.image? 
-            <img 
-              className="w-24 h-24 rounded-full object-cover"
-              src={ user.image } 
-              alt={`${ user.firstName?? "" } ${ user.lastName?? "" }`} />
-            :
-            <div 
-              key="profilePictureBg"
-              className="bg-[#EEEEEE] rounded-full w-24 h-24" />
-          }
+          <img 
+            className="w-24 h-24 rounded-full object-cover"
+            src={ user.image } 
+            alt={`${ user.firstName?? "" } ${ user.lastName?? "" }`} />
         </div>
-        { (!!user.firstName && !!user.lastName ) ? 
+      ) }
+        { (!!user.firstName && !!user.lastName ) &&
           <h1 className="mb-2 leading-[1.25] font-bold text-dark-grey text-[32px] text-center">{ user.firstName?? "" } { user.lastName?? "" }</h1>
-          :
-          <div
-            key="profileNameBg"
-            className="bg-[#EEEEEE] w-[160px] h-4 rounded-2xl mx-auto mb-3"/>
         }
-        { !!user.email ?
-          <p className="text-grey break-words">{ user.email }</p>
-          :
-          <div 
-            key="profileEmailBg"
-            className="bg-[#EEEEEE] w-[72px] h-2 rounded-2xl mx-auto" />
-        }
+        { !!user.email && <p className="text-grey break-words">{ user.email }</p> }
       </div>
       <ul>
         { renderLinks() }
